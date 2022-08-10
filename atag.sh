d058496f8bbd05d232bb6e61bf96314401abc2e3
Hmz-x <hamzamusic34@gmail.com>
2022-08-10 13:40:36 +0300

    why won't it tag

--- atag.sh
+++ atag.sh
@@ -7,8 +7,7 @@ PROGRAM_VERSION="1.0"
 PROGRAM_LICENSE="GNU GPLv3"
 PROGRAM_HELP=\
 "usage: ${PROGRAM_NAME} -a|--audio-input AUDIO_FILE -t|--text-input TEXT_FILE 
-[--album ALBUM_NAME] [--artist ARTIST_NAME] [--cover COVER_FILE]"
-
+[--album ALBUM_NAME] [--artist ARTIST_NAME] [--year YEAR] [--cover COVER_FILE]"
 
 msg_n_exit()
 {
@@ -22,7 +21,31 @@ check_dependencies()
 	[ -z "$(command -v "id3tool")" ] && msg_n_exit "Unable to locate id3tool binary"
 }
 
-split_audio()
+split_n_tag_track()
+{
+	track_number=$1
+	track_name="$2"
+	starting_timestamp="$3"
+	ending_timestamp="$4"
+
+	out_file="${track_name}.${audio_extension}"
+	
+	# Split track
+	if [ -n "$ending_timestamp" ]; then
+		ffmpeg -vn -y -nostdin -i "$audio_file" -ss "$starting_timestamp" -to \
+			"$ending_timestamp" "$out_file"
+	else
+		ffmpeg -vn -y -nostdin -i "$audio_file" -ss "$starting_timestamp" "$out_file"
+	fi
+	
+	# Tag track
+	id3tool --set-title="$track_name" --set-track=$track_number "$out_file"
+	[ -n "$album_name" ] && id3tool --set-album="$album_name" "$out_file"
+	[ -n "$artist_name" ] && id3tool --set-artist="$artist_name" "$out_file"
+	[ -n "$year_data" ] && id3tool --set-year=$year_data "$out_file"
+}
+
+split_audio_loop()
 {
 	[ -z "$audio_file" ] && msg_n_exit "No audio file given"
 	[ -z "$text_file" ] && msg_n_exit "No text file given"
@@ -31,7 +54,7 @@ split_audio()
 	file_len=$(wc -l "$text_file" | cut -d ' ' -f 1)
 
 	line_number=1
-	while read line; do
+	while read -r line; do
 		#time_stamp="$(echo "$line" | grep -Ewo \
 			#"([0-9])*([0-9]:)*([0-6])*[0-9]:[0-6][0-9]")"
 
@@ -48,25 +71,23 @@ split_audio()
 			prev_timestamp="$(echo "$prev_line" | cut -d ' ' -f 1)"
 			prev_name="$(echo "$prev_line" | cut -d ' ' -f 2-)"
 
-			#ffmpeg -i "$audio_file" -ss "$prev_timestamp" -t
 			echo "$prev_name: $prev_timestamp - $timestamp"
-			#if ((i < file_len-1)); then
-				#echo "$prev_timestamp - $timestamp"
-			#else
-				#echo "$prev_timestamp - "
-			#fi
+			split_n_tag_track $line_number "$prev_name" "$prev_timestamp" "$timestamp"
 		fi
 
-		#echo "timestamp: $timestamp - name: $name"
 		((++line_number))
 	done < "$text_file"
 
 	echo "$name: $timestamp - EOF"
+	split_n_tag_track $line_number "$name" "$timestamp"
 
 	rm "$tmp_data_file"
 }
 
 parse_opts(){
+	# Default values
+	audio_extension="mp3"
+
 	# Parse and evaluate each option one by one 
 	while [ "$#" -gt 0 ]; do
 		case "$1" in
@@ -90,6 +111,10 @@ parse_opts(){
 				artist_name="$2" 
 				[ -z "$artist_name" ] && msg_n_exit "No artist name given"
 				shift;;
+			--year)
+				year_data="$2" 
+				[ -z "$year_data" ] && msg_n_exit "No year data given"
+				shift;;
 			--cover)
 				cover_file="$2"
 				[ -r "$text_file" ] || msg_n_exit "Unable to read cover file $cover_file"
@@ -102,4 +127,4 @@ parse_opts(){
 
 check_dependencies
 parse_opts "$@"
-split_audio
+split_audio_loop
